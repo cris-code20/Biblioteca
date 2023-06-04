ba37722e58224965f7f23699e9d9ca0d527dba6b
cris <cris.cadosh@gmail.com>
2023-06-04 10:36:24 -0400

    agregamos los baseEntity

---
+++ Biblioteca.Infrestructure/Core/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Infrestructure.Core
+{
+    internal class BaseEntity
+    {
+        public int Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
