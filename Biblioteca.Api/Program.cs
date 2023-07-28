2512f307db3da2ca9ace3040441ad9d1d5d775d5
Wesley-Martinez14 <riwes1403@gmail.com>
2023-07-28 17:15:30 -0400

    <

--- Biblioteca.Api/Program.cs
+++ Biblioteca.Api/Program.cs
@@ -1,35 +1,42 @@
 using Biblioteca.Application.Contract;
 using Biblioteca.Application.Service;
 using Biblioteca.Infrestructure.Context;
+using Biblioteca.Infrestructure.Entities;
 using Biblioteca.Infrestructure.Interface;
 using Biblioteca.Infrestructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<Bibliotecacontext>(options =>options.UseSqlServer
-                                                 (builder.Configuration.GetConnectionString("BibliotecaContext")));
-builder.Services.AddTransient<ILector, LectorRepositories>();
-builder.Services.AddTransient<ILectorService, LectorService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<Bibliotecacontext>(options => options.UseSqlServer
+                                                         (builder.Configuration.GetConnectionString("BibliotecaContext")));
+        builder.Services.AddTransient<ILector, LectorRepositories>();
+        builder.Services.AddTransient<ILectorService, LectorService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
